7192fcdd935e81aa33032c9fbeb05163d7a2e523
desenvolvimento2.ib3 <desenvolvimento2@grupoib3.com.br>
2019-09-29 11:31:34 -0400

    SideMenu e TopoMenu

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,10 +1,12 @@
 function EnableMenu(){
     var lMenu = document.getElementById("enablemenu");
-    var ul_menu_r = document.getElementById("menu_right");
+    var SideMenu = document.getElementById("sidemenu");
     
     if(lMenu.checked){
-        ul_menu_r.style.transform = "translateX(100%)";
+        SideMenu.style.transform = "translateX(100%)";
     }else{
-        ul_menu_r.style.transform = "translateX(0%)";
+        SideMenu.style.transform = "translateX(0%)";
     }
+
+    console.log("Enable")
  }
