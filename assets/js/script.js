84262730d5c0d98f96f4e0503536c613770d05d8
desenvolvimento2.ib3 <desenvolvimento2@grupoib3.com.br>
2019-10-01 01:30:34 -0400

    Ajuste pasta

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,9 @@ function EnableMenu(){
 
  function fSelectMenu(args){
      switch(args){
-         case 0: $('#main_corpo').load("inicio.html"); break;
-         case 1: $('#main_corpo').load("disciplinas.html"); break;
-         case 2: $('#main_corpo').load( "professores.html"); break;
-         case 3: $('#main_corpo').load("contato.html"); break;
+         case 0: $('#main_corpo').load("pages/inicio.html"); break;
+         case 1: $('#main_corpo').load("pages/disciplinas.html"); break;
+         case 2: $('#main_corpo').load("pages/professores.html"); break;
+         case 3: $('#main_corpo').load("pages/contato.html"); break;
      }    
 };
