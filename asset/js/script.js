e2b8f066a02b32046e4b0a467cc50b9041bb23c4
desenvolvimento2.ib3 <desenvolvimento2@grupoib3.com.br>
2019-10-01 00:47:59 -0400

    pages

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,5 @@
+var InPage;
+
 function EnableMenu(){
     var lMenu = document.getElementById("enablemenu");
     var SideMenu = document.getElementById("sidemenu");
@@ -7,7 +9,14 @@ function EnableMenu(){
     }else{
         SideMenu.style.transform = "translateX(0%)";
     }
+ };
 
-    console.log("Enable")
- }
+ function fSelectMenu(args){
+     switch(args){
+         case 0: $('#main_corpo').load("inicio.html"); break;
+         case 1: $('#main_corpo').load("disciplinas.html"); break;
+         case 2: $('#main_corpo').load( "professores.html"); break;
+         case 3: $('#main_corpo').load("contato.html"); break;
+     }    
+};
 
